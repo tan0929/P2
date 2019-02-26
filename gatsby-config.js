90fd4e79de2664b9ec7b61944d231abf3fc72332
James Tan <tan0929@outlook.com>
2019-02-26 14:13:03 -0800

    add cms option in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,7 +108,12 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
