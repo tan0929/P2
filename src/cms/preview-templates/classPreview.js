3a011b2327a0289dccdd668969ed5064c9cbe668
James Tan <tan0929@outlook.com>
2019-02-26 13:59:27 -0800

    cms preview test 2

--- src/cms/preview-templates/classPreview.js
+++ src/cms/preview-templates/classPreview.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { ClassTemplate } from '../templates/class';
 
 const ClassPreview = ({ entry, widgetFor, widgetsFor, getAsset })=>{
-    const data = entry.getIn('data');
-    console.log(data);
+    
     return(
-        <div
+        <ClassTemplate
+            title={entry.getIn(['data','title'])}
         />
     );
 }
