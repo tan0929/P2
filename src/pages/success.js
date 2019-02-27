bc3e955f29bd1c037d8d505f6155134a56a3a3d0
James Tan <tan0929@outlook.com>
2019-02-27 14:06:14 -0800

    styled form success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -2,17 +2,35 @@ import React from "react"
 import SEO from "../components/seo"
 import Section from '../components/section'
 import styled from 'styled-components';
+import { Main, Secondary } from "../components/text";
 
+
+const Title = styled(Main)`
+    font-size: 40px;
+`;
+
+const Text = styled(Secondary)`
+    font-size: 20px; 
+`;
 
 const Background = styled(Section)`
     background-color: #DBC8BE;
 `;
 
+const TextWrapper = styled.div`
+    max-width: 600px;
+    padding: 30px;
+`;
+
 const NotFoundPage = () => (
   <Background>
     <SEO title="Thanks" />
-    <h1>Thanks</h1>
-    <p>We appreciate that you’ve taken the time to write us. We’ll get back to you very soon. Please come back and see us often.</p>
+    <TextWrapper>
+        <Title>Thanks</Title>
+        <Text>
+            We appreciate that you’ve taken the time to write us. We’ll get back to you very soon. Please come back and see us often.
+        </Text>
+    </TextWrapper>
   </Background>
 )
 
